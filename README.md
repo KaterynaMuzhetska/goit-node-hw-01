8298d6837dd4c914c63d7d498bd455eca36b9d36
KaterynaMuzhetska <74104436+KaterynaMuzhetska@users.noreply.github.com>
2021-09-26 21:32:24 +0300

    HW1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# goit-node-hw-01
+https://monosnap.com/file/tGg5PSeCrpAPlDJ2d0Rl78cLeEPdFz --action list
+https://monosnap.com/file/8tcGvXcokZ4KCjyBgGzfc9qHQ19LGK --action get
+https://monosnap.com/file/JFMPERQD8zSbXDfijLVVd4kDhShGOW --action add
+https://monosnap.com/file/7Hy1GbiXQ897CZAxMiSoeQp5oy1GXZ --action remove
---
+++ contacts.js
@@ -0,0 +1,69 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  const allContacts = await fs.readFile(contactsPath, "utf8");
+  const parsedAllContacts = JSON.parse(allContacts);
+  console.table(parsedAllContacts);
+  return parsedAllContacts;
+}
+
+async function getContactById(id) {
+  const contactId = isNaN(id) ? id : Number(id);
+
+  const allContacts = await fs.readFile(contactsPath, "utf8");
+  const parsedContacts = JSON.parse(allContacts);
+  const contactById = parsedContacts.find(
+    (contact) => contact.id === contactId
+  );
+  if (!contactById) {
+    console.log("There is no contact with such an id!");
+    return;
+  }
+  console.log(contactById);
+  return contactById;
+}
+
+async function removeContact(id) {
+  const contactId = isNaN(id) ? id : Number(id);
+  const allContacts = await fs.readFile(contactsPath, "utf8");
+  const parsedContacts = JSON.parse(allContacts);
+  const deleteContactById = JSON.stringify(
+    parsedContacts.filter((contact) => contact.id !== contactId)
+  );
+  await fs.writeFile(contactsPath, deleteContactById, "utf8");
+  console.log(`Contact with id ${contactId} deleted`);
+}
+
+async function addContact(name, email, phone) {
+  if (!name || !email || !phone) {
+    console.log("Input: name, email, phone!");
+    return;
+  }
+  const allContacts = await fs.readFile(contactsPath, "utf8");
+  const parsedPrevUsers = JSON.parse(allContacts);
+
+  const user = {
+    id: v4(),
+    name,
+    email,
+    phone,
+  };
+
+  const allUsers = parsedPrevUsers.concat([user]);
+  const allUsersJSON = JSON.stringify(allUsers);
+  fs.writeFile(contactsPath, allUsersJSON, "utf8");
+  console.log(`Contact with name ${name} is added`);
+}
+
+const contacts = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
+
+module.exports = contacts;
